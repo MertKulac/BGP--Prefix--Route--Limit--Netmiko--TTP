1cde71e1bab674d3d34c71fc569676890aadd308
Mert Kulaç <96883175+MertKulac@users.noreply.github.com>
2022-02-17 18:26:49 +0300

    Update customer_route_limit.py

--- customer_route_limit.py
+++ customer_route_limit.py
@@ -1,57 +1,113 @@
-def policy():
-        ExcelExport = [["Description", "Interface", "Input_Policy","Output_Policy"]]
-        ciscoasr9k = { "host" : "92.44.0.22","username" :"NPESCRIPT","password" :"RYXga421","port" : 22,"device_type" : "cisco_xr",}
-        conn = ConnectHandler(**ciscoasr9k)
-        conn.enable()
-        #output = conn.send_command_timing('show run interface')
-        with open("show_run.txt") as f:
-                data_to_parse= f.read()
+from netmiko import ConnectHandler
+from getpass import getpass
+from pprint import pprint
+from ttp import ttp
+from genie.testbed import load
+from pprint import pprint
+import json
+import time
+from multiprocessing.dummy import Pool as ThreadPool
+from netmiko import Netmiko
 
-        #print(data_to_parse)
+        #**************************************************************************************************************************
 
-        ttp_template = """
-interface Bundle-Ether{{ interface_Bundle | split(".") }}
- description {{ Description }}
- service-policy input {{ qos_input }}
- service-policy output {{ qos_output}}
+with open("user_pass.txt", "r") as f5:
+        user_pass = f5.readlines()
+
+for list_user_pass in user_pass:
+        if "username" in list_user_pass:
+                username = list_user_pass.split(":")[1].strip()
+        if "password" in list_user_pass:
+                password = list_user_pass.split(":")[1].strip()
+
+def _ssh_(nodeip):
+        try:
+                huawei = {
+                        'device_type': 'huawei', 'ip': nodeip, 'username':
+                        username, 'password': password, }
+                con = Netmiko(**huawei)
+                print(nodeip.strip() + "  " + "basarili giris")
+        except Exception as e:
+                print(e)
+                f_3.write(nodeip.strip() + "\n")
+                return
+
+        #**************************************************************************************************************************
+        
+        data_to_parse_0 = con.send_command_timing('display ip vpn-instance | ignore-case i Customer_A') 
+
+        print(data_to_parse_0)
+
+        ttp_template_0 ="""
+  {{Customer_Name}}                             {{nodeip}}     {{IPV4}}
 """
-        # create parser object and parse data using template:
+        parser_0 = ttp(data=data_to_parse_0, template=ttp_template_0)
+        parser_0.parse()
+
+        #print result in JSON format
+        results_0 = parser_0.result(format='json')[0]
+        print(results_0)
+
+        #str to list **convert with json.loads
+        result_0 = json.loads(results_0)
+        print(result_0[0]["Customer_Name"])
+
+        #**************************************************************************************************************************
+        data_to_parse = con.send_command_timing("display current-configuration configuration vpn-instance  {}".format(result_0[0]["Customer_Name"]))
+        print(data_to_parse)
+
+        ttp_template ="""
+  {{routing-table}} limit {{ total_number | DIGIT }} {{total_number2}}
+
+"""
         parser = ttp(data=data_to_parse, template=ttp_template)
         parser.parse()
 
-        # print result in JSON format
-
+        #print result in JSON format
         results = parser.result(format='json')[0]
+        print(results)
+
+        #str to list **convert with json.loads
         result = json.loads(results)
+        print(result)
 
-        print(result[0])
+        #**************************************************************************************************************************
 
-        #for i in result[0]:
-                #print(int(i["interface_Bundle"][0]))
+        data_to_parse_2 = con.send_command_timing('dis ip routing-table vpn-instance' + " " + result_0[0]["Customer_Name"] + " " + " statistics | i Summary Prefixes")
 
+        print(data_to_parse_2)
 
-        for i in result[0]:
-                if len(i["interface_Bundle"]) > 1 and int(i["interface_Bundle"][1]) > 200 and "Description" in i and i["interface_Bundle"][1] != "311" and i["interface_Bundle"][1] !=  "202"  and i["interface_Bundle"][1] !=  "203"  and i["interface_Bundle"][1] !=  "204"  and i["interface_Bundle"][1] !=  "205"  and i["interface_Bundle"][1] !=  "220"  and i["interface_Bundle"][1] !=  "227"  and i["interface_Bundle"][1] !=  "262"  and i["interface_Bundle"][1] !=  "276"  and i["interface_Bundle"][1] !=  "280"  and i["interface_Bundle"][1] !=  "300"  and i["interface_Bundle"][1] !=  "308"  and i["interface_Bundle"][1] !=  "309"  and i["interface_Bundle"][1] !=  "310"  and i["interface_Bundle"][1] !=  "322"  and i["interface_Bundle"][1] !=  "324"  and i["interface_Bundle"][1] !=  "340"  and i["interface_Bundle"][1] !=  "341"  and i["interface_Bundle"][1] !=  "347"  and i["interface_Bundle"][1] !=  "350"  and i["interface_Bundle"][1] !=  "356"  and i["interface_Bundle"][1] !=  "405"  and i["interface_Bundle"][1] !=  "1225"  and i["interface_Bundle"][1] !=  "1474"  and i["interface_Bundle"][1] !=  "1632"  and i["interface_Bundle"][1] !=  "320"  and i["interface_Bundle"][1] !=  "747" :
-                        if "qos_input" not in i:
-                                interface_Bundle2 = "interface_Bundle"+str(i["interface_Bundle"][0]) +"."+ str(i["interface_Bundle"][1])
-                                #ExcelExport.append([i["Description"],interface_Bundle2,i["qos_input"],i["qos_output"]])
-                                ExcelExport.append([i["Description"],interface_Bundle2,"input policy değer yoktur", "output policy değer yoktur"])
+        ttp_template_2 ="""
+Summary Prefixes : {{ used_number | DIGIT }}
+"""
 
+        parser2 = ttp(data=data_to_parse_2, template=ttp_template_2)
+        parser2.parse()
 
+        #print result in JSON format
+        results2 = parser2.result(format='json')[0]
+        print(results2)
 
-        XLSExport(ExcelExport, "Policy", "policy1.xlsx")
-        SendMailwAttachment_(ExcelExport, "policy1.xlsx")
+        #str to list **convert with json.loads
+        result2 = json.loads(results2)
+        print(result2[0]["used_number"])
 
+        #**************************************************************************************************************************
 
-#schedule.every(10).minutes.do(policy)
-#schedule.every().hour.do(policy)
-#schedule.every().day.at("07:59").do(policy)
-#schedule.every().monday.do(policy)
-schedule.every().monday.at("08:00").do(policy)
-#schedule.every().minute.at(":20").do(policy)
+        result3 = (int(result2[0]["used_number"]) / int(result[0]["total_number"])) * 100
+        print(int(result3))
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+        with open("vrf_limit_result.txt", "a") as f:
+                f.write("Customer_Result" +"_" + nodeip +"==>" + str(result3)+ "\n")
+                f.close()
 
-policy()
+        #**************************************************************************************************************************
+
+f_2 = open("ip_list.txt", "r")
+ip_list = f_2.readlines()
+f_2.close()
+f_3 = open("Ssh_unconnected_2.txt", "w")
+
+# Therading method
+myPool = ThreadPool(100)
+result = myPool.map(_ssh_, ip_list)
